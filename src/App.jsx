c07f9ec835a670c2dfd6f7e6baf42fd1092d83c4
AfrinRai <islamrai13@gmail.com>
2024-04-03 01:40:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+// import DaisyNav from './components/DaisyNav/DaisyNav'
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl bg-yellow-500'>Vite + React</h1>
+      <NavBar></NavBar>
+      {/* <DaisyNav></DaisyNav> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Link from "../Link/Link";
+import { RiMenu2Line } from "react-icons/ri";
+import { RxCross2 } from "react-icons/rx";
+
+const NavBar = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const routes = [
+        { id: 1, path: "/home", name: "Home" },
+        { id: 2, path: "/about", name: "About" },
+        { id: 3, path: "/services", name: "Services" },
+        { id: 4, path: "/products", name: "Products" },
+        { id: 5, path: "/contact", name: "Contact" }
+      ];
+      
+    return (
+        <nav className=" pl-5 bg-gray-500">
+            <div onClick={() => setOpen(!open)} className="text-2xl md:hidden">
+                {
+                    open === true ? <RxCross2></RxCross2> : <RiMenu2Line></RiMenu2Line>
+                }
+            </div>
+            <ul className={`
+                md:flex absolute bg-black text-white duration-1000 md:static
+                ${open ? 'top-16' : '-top-60'}
+                `}>
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
